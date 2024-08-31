a92493af212f975c1dd2967aa79bdbb61a146d81
Baheer Noori <96612910+NarutBaheer@users.noreply.github.com>
2024-09-01 01:36:54 +0430

    Update app.py

--- Assignment2/Q1: Text Generation Application/app.py
+++ Assignment2/Q1: Text Generation Application/app.py
@@ -1,1 +1,22 @@
+from flask import Flask, render_template, request
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+app = Flask(__name__)
+
+model_name = "bigscience/bloom"
+model = AutoModelForCausalLM.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+@app.route('/', methods=['GET', 'POST'])
+def generate_text():
+    if request.method == 'POST':
+        prompt = request.form['prompt']
+        inputs = tokenizer(prompt, return_tensors='pt')
+        outputs = model.generate(inputs['input_ids'], max_length=150)
+        generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        return render_template('index.html', prompt=prompt, generated_text=generated_text)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
